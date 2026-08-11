84d72ef6035a753c446b64ea40456d5b283c075f
anaclaratc3 <trindadeanaclara8@gmail.com>
2026-08-11 16:19:50 -0300

    feat: implementa controller e rotas de transacoes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,17 @@ const cors = require('cors');
 require('dotenv').config();
 
 const categoriaRoutes = require('./routes/categoriaRoutes');
+const transacaoRoutes = require('./routes/transacaoRoutes');
 
 const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // Permite receber JSON no req.body
+app.use(express.json());
 
 // Rotas da API
 app.use('/api/categorias', categoriaRoutes);
+app.use('/api/transacoes', transacaoRoutes);
 
 // Healthcheck
 app.get('/healthcheck', (req, res) => {
@@ -20,5 +22,5 @@ app.get('/healthcheck', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(` Servidor rodando na porta http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
